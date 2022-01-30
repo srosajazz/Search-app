ca5b70737d488304551bc425a1936e78e8d55ec1
Sergio Rosa <scarlosjazz@gmail.com>
2022-01-29 20:12:04 -0500

    backend

---
+++ backend/src/test/java/com/sergiorosa/demo/SearchappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiorosa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
