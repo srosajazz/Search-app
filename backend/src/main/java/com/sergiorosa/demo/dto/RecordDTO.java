e6e7ded96c4d57e54d9f2509a609dfe36ef9ee4f
Sergio Rosa <scarlosjazz@gmail.com>
2022-02-08 21:26:06 -0500

    CyclicSort & LeetCode questions

---
+++ backend/src/main/java/com/sergiorosa/demo/dto/RecordDTO.java
@@ -0,0 +1,30 @@
+package com.sergiorosa.demo.dto;
+
+import java.time.Instant;
+
+import com.sergiorosa.demo.entities.enums.Platform;
+
+
+
+public class RecordDTO {
+	
+	private Long id;
+	private Instant moment;
+	private String name;
+	private Integer age;
+	private String gameTitle;
+	private Platform gamePlatform;
+	private String genreName;
+	
+	public RecordDTO() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
